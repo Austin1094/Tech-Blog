936af6b8a3b0b064026a718b68ba3b98c4a79b88
Austin1094 <Austin.Adams1094@gmail.com>
2023-07-09 21:54:10 -0400

    js files being worked on

---
+++ public/js/comment.js
@@ -0,0 +1,29 @@
+const commentFormHandler = async function (event) {
+    event.preventDefault();
+
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    if (comment_text) {
+        const response = await fetch('/api/comment', {
+            method: 'POST',
+            body: JSON.stringify({
+                comment_text,
+                post_id
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('#new-comment-form').addEventListener('submit', commentFormHandler);

bef2476c7911eba03a97469c1fab2832eb0ba780
Austin1094 <Austin.Adams1094@gmail.com>
2023-07-10 21:49:29 -0400

    Finished up js files for now

---
+++ public/js/login.js
@@ -0,0 +1,20 @@
+const loginFormHandler = async function (event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
